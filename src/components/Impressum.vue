04f7ad75e2a08b83123d63401e69c5f852a77abf
Tamara Trajkovski <pa23l002@technikum-wien.at>
2024-10-19 00:40:23 +0200

    Created Impressum (Text)

---
+++ src/components/Impressum.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+  name: 'Impressum'
+};
+</script>
+
+
+<template>
+  <div class="impressum">
+    <h4>Impressum</h4>
+    <p>
+      <b>Informationen und Offenlegung gemäß &sect;5 (1) ECG, &sect; 25 MedienG, &sect; 63 GewO und &sect; 14 UGB</b>
+    </p>
+    <p>
+      <b>Webseitenbetreiber:</b> Boutique Hotel Gmbh<br>
+      <b>Firmenbuchnummer:</b> 123456n<br>
+      <b>Firmenbuchgericht:</b> Handelsgericht Wien
+    </p>
+    <p>
+      <b>Anschrift:</b> Höchstädtplatz 6, 1200 Wien
+    </p>
+    <p>
+      <b>UID-Nr:</b> ATU01010101<br>
+      <b>Gewerbeaufsichtbehörde:</b> Bezirkshauptmannschaft Wien<br>
+      <b>Mitgliedschaften:</b> Mitglied der WKÖ
+    </p>
+    <p>
+      <b>Kontaktdaten:</b><br>
+      Telefon: +43 1 3141592<br>
+      Fax: +43 1 3141592 404<br>
+      Email: <a href="mailto:nerds@bitsandbytes.at">nerds@bitsandbytes.at</a>
+
+    </p>
+    <p>
+      <b>Anwendbare Rechtsvorschrift:</b> <a href="http://www.ris.bka.gv.at" target="_blank">www.ris.bka.gv.at</a><br>
+    </p>
+    <p>
+      <b>Urheberrecht:</b> Die Inhalte dieser Webseite unterliegen, soweit dies rechtlich möglich ist, diversen
+      Schutzrechten (z.B dem Urheberrecht). Jegliche Verwendung/Verbreitung von bereitgestelltem Material, welche
+      urheberrechtlich untersagt ist, bedarf schriftlicher Zustimmung des Webseitenbetreibers.
+    </p>
+    <p>
+      <b>Haftungsausschluss:</b> Trotz sorgfältiger inhaltlicher Kontrolle übernimmt der Webseitenbetreiber dieser
+      Webseite keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich
+      deren Betreiber verantwortlich. Sollten Sie dennoch auf ausgehende Links aufmerksam werden, welche auf eine
+      Webseite mit rechtswidriger Tätigkeit/Information verweisen, ersuchen wir um dementsprechenden Hinweis, um diese
+      nach § 17 Abs. 2 ECG umgehend zu entfernen. Die Urheberrechte Dritter werden vom Betreiber dieser Webseite mit
+      größter Sorgfalt beachtet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um
+      einen entsprechenden Hinweis. Bei Bekanntwerden derartiger Rechtsverletzungen werden wir den betroffenen Inhalt
+      umgehend entfernen.
+    </p>
+    <p>
+      <b>Beteiligungsverhältnisse der Gesellschaft:</b> Marina Lanxinger (33,33%), Ricarda Pfaffenbichler (33,33%),
+      Tamara Trajkovski (33,33%)<br>
+      <b>Geschäftsführerinnen:</b> Marina Lanxinger, Ricarda Pfaffenbichler, Tamara Trajkovski
+    </p>
+    <p>
+      Quelle: <b><a href="https://www.fairesrecht.at/kostenlos-impressum-erstellen-generator.php" target="_blank">Impressum
+      Generator Österreich</a></b>
+    </p>
+  </div>
+</template>
+
+
+<style scoped>
+
+.impressum {
+  margin: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
