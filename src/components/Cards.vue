6129f1085a1d27b43fbc30a78332d923982085fc
ricarda <ricarda.karli@gmail.com>
2024-10-20 17:58:05 +0200

    cards anpassungen mit Containern in richtiger Reihenfolge

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,50 +1,51 @@
 <script>
 export default {
-  name: "Cards"
-
-}
+  name: "HotelCard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    imgAlt: {
+      type: String,
+      default: "Room Image",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      default: "#",
+    },
+    buttonText: {
+      type: String,
+      default: "View Details",
+    },
+  },
+};
 </script>
 
 <template>
-  <b-container class="mt-4">
-    <b-row>
-      <b-col md="6" lg="6">
-        <b-card
-            title="Card 1 Title"
-            img-src="https://picsum.photos/200/150"
-            img-alt="Image 1"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mb-2"
-        >
-          <b-card-text>
-            This is the content of the first card. It includes a quick example text to build on the card title.
-          </b-card-text>
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="6" lg="6">
-        <b-card
-            title="Card 2 Title"
-            img-src="https://picsum.photos/200/150"
-            img-alt="Image 2"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mb-2"
-        >
-          <b-card-text>
-            This is the content of the second card. It also has some text to describe the card content.
-          </b-card-text>
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container>
+  <b-card
+      :title="title"
+      :img-src="imgSrc"
+      :img-alt="imgAlt"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+  >
+    <b-card-text>
+      {{ description }}
+    </b-card-text>
+    <b-button :href="link" variant="primary">{{ buttonText }}</b-button>
+  </b-card>
 </template>
-
 
 
 
--- src/views/landingView.vue
+++ src/views/landingView.vue
@@ -1,16 +1,58 @@
 <script>
 import Cards from "@/components/cards.vue";
+import HotelCard from "@/components/Cards.vue";
 
 export default {
   name: "landingView",
-  components: {Cards}
+  components: {HotelCard, Cards}
 }
 </script>
 
 <template>
-  <main>
-    <cards />
-  </main>
+  <b-container class="mt-4">
+    <!-- Erste Zeile mit zwei Cards -->
+    <b-row>
+      <b-col md="6" lg="6">
+        <HotelCard
+            title="Alan Turing"
+            description="Alan Turing war ein britischer Mathematiker, Logiker und Informatiker, der als einer der Väter der modernen Computerwissenschaft gilt."
+            img-src="https://picsum.photos/200/150?random=1"
+            link=""
+            button-text="Jetzt buchen" />
+      </b-col>
+
+      <b-col md="6" lg="6">
+        <HotelCard
+            title="Grace Hopper"
+            description="Grace Hopper war eine amerikanische Computerwissenschaftlerin und Navy-Offizierin, die als eine der Pionierinnen der Informatik gilt."
+            img-src="https://picsum.photos/200/150?random=2"
+            link=""
+            button-text="Jetzt buchen" />
+      </b-col>
+    </b-row>
+
+    <!-- Zweite Zeile mit zwei Cards -->
+    <b-row>
+      <b-col md="6" lg="6">
+        <HotelCard
+            title="Ada Lovelace"
+            description="Ada Lovelace, geboren 1815 in London, war eine brillante Mathematikerin und wird oft als die erste Programmiererin der Welt bezeichnet."
+            img-src="https://picsum.photos/200/150?random=3"
+            link=""
+            button-text="Jetzt buchen" />
+      </b-col>
+
+      <b-col md="6" lg="6">
+        <HotelCard
+            title="Margaret Hamilton"
+            description="Margaret Hamilton war eine amerikanische Computerwissenschaftlerin, die die Software für die Apollo-Raumfahrtprogramme entwickelte."
+            img-src="https://picsum.photos/200/150?random=4"
+            link=""
+            button-text="Jetzt buchen" />
+      </b-col>
+    </b-row>
+  </b-container>
+
 
 </template>
 
