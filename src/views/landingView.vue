d137ec2277b20d991bdb5b996c71bccc15e759ab
ricarda <ricarda.karli@gmail.com>
2024-10-20 16:57:43 +0200

    Create landing view file

---
+++ src/views/landingView.vue
@@ -0,0 +1,19 @@
+<script>
+import Cards from "@/components/cards.vue";
+
+export default {
+  name: "landingView",
+  components: {Cards}
+}
+</script>
+
+<template>
+  <main>
+    <cards />
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
