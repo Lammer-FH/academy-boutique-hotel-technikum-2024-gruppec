a18822691ad50f92db09c59f92da8d8109803e53
ricarda <ricarda.karli@gmail.com>
2024-11-03 18:24:02 +0100

    Room-Card Komponente angelegt & auf roomsView integriert.

---
+++ src/components/CardsRooms.vue
@@ -0,0 +1,56 @@
+<script>
+
+export default {
+  name: "CardsRooms",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    imgAlt: {
+      type: String,
+      default: "Room Image",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      default: "#",
+    },
+    buttonText: {
+      type: String,
+      default: "View Details",
+    },
+  },
+};
+
+</script>
+
+<template>
+  <b-card
+    :title="title"
+    :img-src="imgSrc"
+    :img-alt="imgAlt"
+    img-top
+    tag="article"
+    style="max-width: 50rem;"
+    class="mb-2"
+    >
+    <b-card-text>
+      {{ description }}
+    </b-card-text>
+    <b-button :href="link" variant="primary">{{ buttonText }}</b-button>
+  </b-card>
+
+</template>
+
+<style scoped>
+
+
+</style>
--- src/views/roomsView.vue
+++ src/views/roomsView.vue
@@ -1,16 +1,17 @@
 <script>
 import {defineComponent} from "vue";
-import HotelCard from "@/components/Cards.vue";
-import Zimmer7BernersLee from "@/assets/Karussell/Zimmer7_Berners-Lee.png";
+import CardsRooms from "@/components/CardsRooms.vue"
 import Footer from "@/components/Footer.vue";
 
+import Zimmer_2_GraceHopper from "@/assets/RoomImages/Zimmer_2_GraceHopper.webp";
+
 const roomImages = [
-  {src: Zimmer7BernersLee, alt: "Zimmer7"},
+  {src: Zimmer_2_GraceHopper, alt: "Zimmer2"},
 ];
 
 export default defineComponent({
   name: 'Rooms',
-  components: {HotelCard, Footer},
+  components: {Footer, roomImages, CardsRooms},
   data() {
     return {
       roomImages: roomImages,
@@ -21,38 +22,53 @@ export default defineComponent({
 
 <template>
 
-  <div class="cards-container">
-    <b-row>
-      <b-col cols="12" md="4" lg="4">
-        <HotelCard
-        title="Tim Berners-Lee"
-        description="Ein modernes, technologiegetriebenes Hotelzimmer, das die Vision von Tim Berners-Lee, dem Erfinder des World Wide Web, feiert. Mit klaren Linien und einer eleganten, minimalistischen Ästhetik bietet das Zimmer eine harmonische Mischung aus Komfort und Funktionalität."
-        :imgSrc="roomImages[0].src"
-        imgAlt="Bild von Tim Berners-Lee"
-        button-text="Verfügbarkeit prüfen"
-        />
-      </b-col>
-    </b-row>
-
+  <div class="room-info">
+    <h3>Unserer Zimmer</h3>
+    <h2>Jeder Raum ein Unikat</h2>
+    <p>Jedes unserer Zimmer ist nach einer bedeutenden Persönlichkeit aus der Computergeschichte benannt und liebevoll gestaltet,
+      um die Inspiration und Innovation dieser Pioniere widerzuspiegeln.
+      Von Grace Hopper bis Alan Turing – jedes Zimmer bietet eine individuelle Atmosphäre, die sowohl Komfort als auch einen Hauch von Nerd-Kultur vermittelt.
+      Wir laden Sie ein, Ihren inneren Nerd zu entdecken, während Sie in einem unserer thematischen Zimmer verweilen und
+      die Verbindung zwischen Geschichte und Technologie hautnah erleben!
+    </p>
   </div>
+
+<div class="cards-container">
+  <b-row>
+    <CardsRooms
+        title="Grace Hopper"
+        description="Ein modernes, technologiegetriebenes Hotelzimmer, das die Vision von Grace Hopper, einer der bedeutendsten Computerwissenschaftlerinnen des 20. Jahrhunderts, feiert. Inspiriert von ihrem bahnbrechenden Beitrag zur Programmierung und dem Konzept der Programmiersprachen kombiniert das Zimmer innovative Technologien mit einem zeitgemäßen Design. Klare Linien und ein stilvolles, funktionales Interieur schaffen eine einladende Atmosphäre, die Kreativität und Produktivität fördert. Mit intelligenten Lösungen und modernsten Annehmlichkeiten bietet das Zimmer eine perfekte Symbiose aus Komfort und technologischem Fortschritt."
+        :img-src="roomImages[0].src"
+        img-alt="Bild von Zimmer Grace Hopper"
+        button-text="Verfügbarkeit prüfen">
+    </CardsRooms>
+
+  </b-row>
+</div>
 
   <Footer>
   </Footer>
-
 </template>
 
 <style scoped>
 
+.room-info {
+  max-width: 1000px;
+  margin: 50px auto;
+  text-align: left;
+  overflow-wrap: break-word;
+}
+
+.room-info h2, .room-info h3 {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
 .cards-container {
   display: flex;
   justify-content: center;
-  margin-top: 20px;
-}
 
-/* Stil für einzelne Karten */
-.cards-container .b-col {
-  max-width: 300px; /* Maximale Breite für die Cards */
-  margin: 10px; /* Abstand zwischen den Cards */
+
 }
 
 </style>
